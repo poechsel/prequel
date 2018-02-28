7777c666feebd798fc9a52bc7fbfcac26cce14eb
poechsel <pierre.oechsel@gmail.com>
2018-02-28 11:46:11 +0100

    added a basic repl

--- src/main.ml
+++ src/main.ml
@@ -1,1 +1,16 @@
 let _ = print_string "hello_world\n"
+let parse_line lexbuf = 1;;
+let compute ast = 1;;
+let display request = "hello world"
+
+let repl () = 
+  let lexbuf = Lexing.from_channel stdin
+  in let rec aux () = 
+       let _ = print_string ">> "; flush stdout
+       in let ast = parse_line lexbuf
+       in let request = compute ast
+       in let _ = display request
+       in aux ()
+  in aux ();;
+
+let _ = repl()
