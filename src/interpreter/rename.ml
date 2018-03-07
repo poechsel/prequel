ddccd6689f87cd633197c9a2503730e7de961e98
poechsel <pierre.oechsel@gmail.com>
2018-03-07 09:53:47 +0100

    added the option to rename a table

---
+++ src/interpreter/rename.ml
@@ -0,0 +1,15 @@
+class rename_table (sub : AlgebraTypes.feed_interface) ( name : string ) =
+  object(self)
+    inherit AlgebraTypes.feed_interface
+
+    val sub = sub
+    val name = name
+
+    method next = sub#next
+
+    method reset =
+      sub#reset
+
+    method headers =
+      List.map (fun i -> (Some name, snd i)) sub#headers
+  end 
