1c63a39d7de985ee51921c9331ca3c0493798882
Romain Liautaud <romain.liautaud@ens-lyon.fr>
2018-03-18 19:36:56 +0100

    Added a few simple tests.

---
+++ tests/basic/select-few.sql
@@ -0,0 +1,13 @@
+SELECT idd, nom FROM "../sources/departements.csv";
+===================================================
+idd,nom
+1,Direction
+2,Recherche
+3,Développement
+4,Marketing
+5,Production
+6,Achats
+7,Comptabilité
+8,RH
+9,Exportations
+10,Administration
