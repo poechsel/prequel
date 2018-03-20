f144354e6d35a7fed9e644dc11ab079f4991f38b
poechsel <pierre.oechsel@gmail.com>
2018-03-20 23:48:09 +0100

    push down selections optimisation

--- src/compiler/optimisationPass.ml
+++ src/compiler/optimisationPass.ml
@@ -15,82 +15,73 @@ let attributes_of_condition cond =
      |> List.sort_uniq Pervasives.compare 
 
 
-let list_sym_diff l1 l2 = 
+let exclusive_join l1 l2 =
   let rec aux l acc = 
     match l with
     | [] -> acc
-    | x::y::tl when x = y -> aux tl acc
-    | x::tl -> aux tl (x::acc)
-  in aux (List.sort Pervasives.compare @@ l1 @ l2) []
+    | x::y::tl when x = y -> aux tl (x::acc)
+    | x::tl -> aux tl acc
+  in 
+  let l1 = List.sort_uniq Pervasives.compare l1 in
+  let l2 = List.sort_uniq Pervasives.compare l2 in
+  let l = List.merge Pervasives.compare l1 l2 in
+  aux l []
 
-let is_sublist l1 l2 =
-  let _ = Printf.printf "%s sublist %s\n" 
-    (String.concat "," (List.map (fun x -> x |> Debug.string_of_header) l1))
-    (String.concat "," (List.map (fun x -> x |> Debug.string_of_header) l2))
-    in
-    (* return true if l1 is a sublist of l2 *)
-  let rec aux l1 l2 = 
-    match l1, l2 with 
-    | [], _ -> true
-    | _, [] -> false
-    | x1::t1, x2::t2 when x1 = x2 ->
-        aux t1 t2
-    | t1, x2::t2 ->
-      aux t1 t2
-  in aux (List.sort Pervasives.compare l1) (List.sort Pervasives.compare l2)
+
+module SetAttributes = Set.Make (struct
+    type t = AlgebraTypes.header
+    let compare = Pervasives.compare
+  end )
 
 let push_down_select query = 
-  let tbl = Hashtbl.create 10 in
-  let _ = MetaQuery.get_headers ~f:(fun uid header -> Hashtbl.add tbl uid (Array.to_list header)) query in
+  let get_headers query =
+    (*TODO optimize, with a hashtbl for exemple*)
+    MetaQuery.get_headers query
+    |> Array.to_list
+    |> SetAttributes.of_list 
+  in 
   let rec push_down can_be_pushed query =
-    (*TODO optimize
-      let header = Hashtbl.find tbl (MetaQuery.get_uid_from_alg query) in*)
-    let header = MetaQuery.get_headers query |> Array.to_list in
-    let _ = 
-      Printf.printf "(%d) %s\n"
-        (MetaQuery.get_uid_from_alg query)
-        (String.concat " " (List.map Debug.string_of_header header)) in
-    let push_further_down, insert_before = 
-      can_be_pushed |> 
-      List.partition (fun (cond, attrs) ->
-         is_sublist attrs header
-        )
+    let analyze_sub query =
+      let header = get_headers query in
+      let push, stay =
+        can_be_pushed 
+        |> List.partition (fun x -> SetAttributes.subset (snd x) header) 
+      in stay, push_down push query
     in 
     let to_insert, req = 
       match query with
       | AlgUnion(u, a, b) ->
-        let i1, a' = push_down push_further_down a in
-        let i2, b' = push_down push_further_down b in
-        list_sym_diff i1 i2, AlgUnion(u, a', b')
+        let i1, a' = analyze_sub a in 
+        let i2, b' = analyze_sub b in
+        exclusive_join i1 i2, AlgUnion(u, a', b')
 
       | AlgMinus(u, a, b) ->
-        let i1, a' = push_down push_further_down a in
-        let i2, b' = push_down push_further_down b in
-        list_sym_diff i1 i2, AlgMinus(u, a', b')
+        let i1, a' = analyze_sub a in 
+        let i2, b' = analyze_sub b in
+        exclusive_join i1 i2, AlgMinus(u, a', b')
       
       | AlgProduct(u, a, b) ->
-        let i1, a' = push_down push_further_down a in
-        let i2, b' = push_down push_further_down b in
-        list_sym_diff i1 i2, AlgProduct(u, a', b')
+        let i1, a' = analyze_sub a in 
+        let i2, b' = analyze_sub b in
+        exclusive_join i1 i2, AlgProduct(u, a', b')
        
       | AlgProjection(u, a, headers) ->
-        let i, a' = push_down push_further_down a in
+        let i, a' = analyze_sub a in
         i, AlgProjection(u, a', headers)
 
       | AlgRenameTable(u, a, name) ->
-        let i, a' = push_down push_further_down a in
+        let i, a' = analyze_sub a in
         i, AlgRenameTable(u, a', name)
 
       | AlgSelect(u, a, filter) ->
-        let attrs = attributes_of_condition filter in
-        let i, a' = push_down ((filter, attrs) :: push_further_down) a in
-        i, a'
+        let attrs = attributes_of_condition filter |> SetAttributes.of_list in
+        let a' = push_down ((filter, attrs) :: can_be_pushed) a in
+        [], a'
 
       | AlgInput(u, str) ->
         can_be_pushed, query
 
-    in insert_before, 
-       List.fold_left (fun a (cond, _) -> AlgSelect(AlgebraTypes.new_uid (), a, cond))
+    in List.fold_left (fun a (cond, _) -> AlgSelect(AlgebraTypes.new_uid (), a, cond))
          req
          to_insert
-  in snd @@ push_down [] query
+  in push_down [] query
