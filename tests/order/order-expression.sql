326bbcdf769a2a950463a09ca840a3b8fa65e6fe
Romain Liautaud <romain.liautaud@ens-lyon.fr>
2018-03-25 23:14:33 +0200

    Test coverage of ORDER BY.

---
+++ tests/order/order-expression.sql
@@ -0,0 +1,25 @@
+SELECT * FROM "tests/sources/projets.csv" proj
+ORDER BY proj.idp + proj.responsable ASC;
+
+[ordered]
+idp, titre, responsable
+2, Capricornus, 5
+9, Scorpius, 5
+13, Cancer, 4
+19, Libra, 8
+3, Virgo, 29
+18, Sagitta, 15
+14, Lyra, 26
+12, Ursa, 38
+10, Argo Navis, 44
+7, Orion, 48
+17, Aquila, 40
+16, Perseus, 44
+1, Andromeda, 68
+5, Aquarius, 79
+11, Gemini, 75
+15, Lupus, 75
+20, Aries, 75
+4, Pegasus, 94
+6, Ophiuchus, 96
+8, Pisces, 99
